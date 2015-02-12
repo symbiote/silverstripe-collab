3157788721d3456afdcd3fe99ee30ed405e578e3
Nathan Glasl <nathan@silverstripe.com.au>
2015-02-12 16:15:24 +1100

    Adding a missing file extension used for the file listing page.

---
+++ code/extensions/TaxonomyFileExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+class TaxonomyFileExtension extends DataExtension {
+
+	private static $many_many = array(
+		'Terms' => 'TaxonomyTerm'
+	);
+
+	public function updateCMSFields(FieldList $fields) {
+		if ($this->owner instanceof Folder) return;
+
+		Requirements::css('mysite/css/taxonomy-file-extension.css');
+
+		$taxonomySourceFunction = function(){
+			$source = TaxonomyTerm::get()->exclude('ParentID', 0);
+			$result = array();
+			if($source->count()){
+				foreach ($source as $term) {
+					$result[$term->ID] = $term->getTaxonomyName() . ": $term->Title";
+				}
+			}
+			asort($result);
+			return $result;
+		};
+		$taxonomySource = $taxonomySourceFunction();
+		$fields->addFieldToTab(
+			'Root.Main',
+			ListBoxField::create('Terms', 'Terms', $taxonomySource, null, null, true)
+				->useAddNew(
+					'TaxonomyTerm',
+					$taxonomySourceFunction,
+					FieldList::create(
+						TextField::create('Name', 'Title'),
+						DropdownField::create('ParentID', 'Parent', TaxonomyTerm::get()->filter('ParentID', 0)->map()->toArray())
+							->setEmptyString('')
+					)
+				)
+		);
+	}
+}
